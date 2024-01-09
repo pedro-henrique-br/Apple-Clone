f22b3a12373e9bbd8a34270ccab6e710d0b84efa
Pedro <phbr10.12@gmail.com>
2024-01-08 22:33:34 -0300

    js button

--- js/main.js
+++ js/main.js
@@ -1,33 +1,25 @@
 // Selecionando elementos
 
 const iphoneImg = document.getElementById("iphone-img")
+const buttons = document.getElementsByClassName("btn");
 
-const btnIphoneVerde = document.getElementById("btn-verde")
-const btnIphonePrateado = document.getElementById("btn-prateado")
-const btnIphoneGrafite = document.getElementById("btn-grafite")
-const btnIphoneAzul = document.getElementById("btn-azul")
-const btnIphoneDourado = document.getElementById("btn-dourado")
 
-btnIphoneVerde.addEventListener ("click", () => {
-  iphoneImg.src = `/assets/iphone_green.jpg`
-  iphoneImg.classList.toggle("active")
-})
+// Transformando o objeto buttons em array com rest parameter 
 
-btnIphoneDourado.addEventListener ("click", () => {
-  iphoneImg.src = `/assets/iphone_golden.jpg`
-  iphoneImg.classList.toggle("active")
-})
+const btn = [...buttons]
 
-btnIphoneGrafite.addEventListener ("click", () => {
-  iphoneImg.src = `/assets/iphone_grafite.jpg`
-  iphoneImg.classList.toggle("active")
-})
 
-btnIphoneAzul.addEventListener ("click", () => {
-  iphoneImg.src = `/assets/iphone_blue.jpg`
-  iphoneImg.classList.toggle("active")
-})
+// Funções 
 
-btnIphonePrateado.addEventListener ("click", () => {
-  iphoneImg.src = `/assets/iphone_silver.jpg`
-})
+btn.forEach((btn) => {
+  btn.addEventListener("click", (e) =>{ /* Evento de click */
+    iphoneImg.src = `/assets/${e.target.id}.jpg`;
+    iphoneImg.classList.add("active");
+    btn.classList.toggle("selected")
+
+    setTimeout(() => { /* SetTimeout para desativar a classe do css */
+      iphoneImg.classList.toggle("active");
+  }, 300)
+  })
+});
+
